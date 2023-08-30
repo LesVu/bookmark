94ab4a6f70f156b8a79dc81a110f1f34f62e87e2
LesVu <66196443+LesVu@users.noreply.github.com>
2023-08-30 06:55:22 +0000

    Test

--- chrome_extension/static/index.js
+++ chrome_extension/static/index.js
@@ -1,8 +1,1 @@
-console.log("Hello World");
-
-let count = 0;
-let button = document.getElementById("click-button");
-button.addEventListener("click", () => {
-  count++;
-  document.getElementById("count").innerHTML = count;
-});
+console.log('Hello World');
